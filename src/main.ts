659a898fca6b821f5343af45216d1105cdf02267
ayodejipy <ay.programmr@gmail.com>
2024-01-05 09:47:54 +0100

    update: graphql setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,35 @@
-import { createApp } from 'vue'
+import { createApp, h, provide } from 'vue'
 import './styles/main.css'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client/core'
 import App from './App.vue'
 import initializeRouter from './router'
+// graphql setup
 
-const app = createApp(App)
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: 'http://localhost:3020/graphql',
+})
+
+const cache = new InMemoryCache()
+
+// create apollo client
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+})
+
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient)
+  },
+
+  render: () => h(App),
+})
 
 initializeRouter(app)
 
