8a28ba05f3f01377137d6af3246c6113b9d2d7f0
ayodejipy <ay.programmr@gmail.com>
2024-01-05 17:11:29 +0100

    update: clean up graphql setup

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,9 @@
-import { createApp, h, provide } from 'vue'
+import { createApp } from 'vue'
 import './styles/main.css'
-import { DefaultApolloClient } from '@vue/apollo-composable'
-import {
-  ApolloClient,
-  createHttpLink,
-  InMemoryCache,
-  gql,
-} from '@apollo/client/core'
 import App from './App.vue'
 import initializeRouter from './router'
 
-// graphql setup
-const httpLink = createHttpLink({
-  uri: 'https://metaphysics-production.artsy.net/v2',
-})
-
-const cache = new InMemoryCache()
-
-// create apollo client
-const apolloClient = new ApolloClient({
-  link: httpLink,
-  cache,
-})
-
-const app = createApp({
-  setup() {
-    provide(DefaultApolloClient, apolloClient)
-  },
-
-  render: () => h(App),
-})
+const app = createApp(App)
 
 initializeRouter(app)
 
